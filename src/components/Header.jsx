a1592502b4b56ecac4cf65e11b5c0b9321f2c812
Physics-Computer <ms_0710_@naver.com>
2024-10-10 22:01:52 +0900

    과제4초안

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
 // Header component
 
 const Header = () => {
-  return <div></div>;
+  return (<div
+    style={{
+      display: 'flex', // Flexbox를 사용하여 자식 요소들을 수평으로 배치
+      alignItems: 'center', // 자식 요소들을 수직 가운데 정렬
+      backgroundColor: '#A60606', // 배경색을 빨간색으로 설정
+      padding: '10px', // 상하 좌우에 10px의 패딩 추가
+        justifyContent: 'space-between', // 자식 요소 간의 간격을 균등하게
+    }}
+  >
+    
+  </div >
+  );
 };
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,29 @@
 // 아래의 import문으로 next/image의 Image 컴포넌트를 사용할 수 있습니다.
-// import Image from "next/image"
+import Image from "next/image"
 
 // 아래의 import문으로 MessageBox 컴포넌트를 사용할 수 있습니다.
-//import MessageBox from "@/components/MessageBox";
+import MessageBox from "@/components/MessageBox";
+
+
 
 export default function Home() {
   return (
     <main>
-      {/* font 적용하는 법 예시 */}
-      <h1 className="font-['Changwon']"></h1>
+      {/* <h1 className="font-size:55px; font-['Changwon']"> 💌 </h1>
+      <h1 className="font-size:32px; font-['Changwon']">연말 편지 우체통 </h1>
+      
+      <h1 className="font-size:32px; font-['Changwon']">Google 로그인하고 쓰기 </h1> */}
+
+      <h1 className="font-size:55px; font-['Changwon'] Header">
+        <p>
+          <span>💌</span>
+          <span>연말 편지 우체통</span>
+          <Image src="/images/image.png" alt="google icon" width={58} height={50}>
+          </Image>
+          <span>Google 로그인하고 쓰기</span>
+        </p>
+
+      </h1>
     </main>
   );
 }
