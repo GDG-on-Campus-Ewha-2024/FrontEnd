ba207b503b65761eff93f7860efe389abbb91d6b
bhyerain <seunghye0307@naver.com>
2024-10-13 14:20:47 +0900

    [Feat] 초기 설정

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,18 @@
 
 export default function Home() {
   return (
-    <main>
-      {/* font 적용하는 법 예시 */}
-      <h1 className="font-['Changwon']"></h1>
-    </main>
+    <div>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center gap-[20px]">
+          <h1 className="text-[50px] font-['Changwon']">연말 편지 우체통</h1>
+          <h5 className="font-['Hakyo']">
+            양승혜님의 연말 편지 우체통으로 00건의 편지가 도착했어요!
+          </h5>
+        </div>
+        <button className="w-[200px] h-[50px] font-['Changwon'] border-[1px] bg-[#B61111] text-white rounded-lg shadow-lg">
+          편지쓰러 가기
+        </button>
+      </div>
+    </div>
   );
 }
