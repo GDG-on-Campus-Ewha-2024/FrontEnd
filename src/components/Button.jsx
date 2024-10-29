56c8162f3eb192e853848fca0e3aaa5c1f744c49
yongaricode <kang061313@gmail.com>
2024-10-29 16:41:05 +0900

    feat: Letter 컴포넌트 생성 및 writeLetter 페이지 구현

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ text }) => {
   return (
     <div>
-      <button className="w-[200px] h-[50px] border-[3px] border-[#8D4343] font-['TTLaundryGothicB'] bg-[#FAA8A8] rounded-[15px] text-[20px]">
+      <button className="w-[200px] h-[50px] border-[3px] border-strong font-['TTLaundryGothicB'] bg-blue-100 rounded-[15px] text-[20px]">
         {text}
       </button>
     </div>
--- src/pages/writeLetter.jsx
+++ src/pages/writeLetter.jsx
@@ -1,9 +1,19 @@
 import Button from "../components/Button.jsx";
+import Letter from "../components/Letter.jsx";
 
 export default function writeLetter() {
   return (
-    <div className="bg-back h-screen w-screen flex justify-center">
-      <Button text="우체통에 넣기" />
+    <div className="bg-back h-screen w-screen flex items-center flex-col">
+      <h3 className="mt-[80px] font-['TTLaundryGothicB'] text-[30px]">
+        💌 용가리님께 편지를 써주세요!
+      </h3>
+      <section className="mt-[50px]">
+        <Letter />
+      </section>
+
+      <section className="mt-[60px]">
+        <Button text="우체통에 넣기" />
+      </section>
     </div>
   );
 }
