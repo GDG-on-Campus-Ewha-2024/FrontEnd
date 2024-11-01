7c8a2160a4052b80d1c36ca0c9ba79fe0908d9ab
yelynnn <lynn1833@ewhain.net>
2024-11-01 15:38:05 +0900

    messagebox 구현

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,7 +1,15 @@
 // MessageBox component
 
-const MessageBox = () => {
-  return <></>;
+const MessageBox = ({ name, message }) => {
+  return (
+    <>
+      <div className="w-[230px] h-[160px] bg-white rounded-lg border-4 border-blue-500 p-4">
+        <span className="font-semibold font-['Hakyo']">From. {name}</span>
+        <br />
+        <span className="my-5 font-['Hakyo'] text-gray-600">{message}</span>
+      </div>
+    </>
+  );
 };
 
 export default MessageBox;
