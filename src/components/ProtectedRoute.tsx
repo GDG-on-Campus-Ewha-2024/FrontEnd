ad62ac4ec083d84c7d2b503db12e2f30081d3170
yongaricode <kang061313@gmail.com>
2024-12-14 18:04:40 +0900

    feat: 라우터 페이지 삭제 및 ProtectedRoute 구현

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,10 +1,26 @@
-import { Navigate } from "react-router-dom";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const isAuthenticated = true;
+  const isAuthenticated = false;
+  const router = useRouter();
+  const redirected = useRef(false);
 
+  // useRef 처음에 안썼더니, useEffect가 두 번 실행돼 alert이 두 번 뜨는 상황이 발생
+  // 검색해보니, next는 개발 모드에서 React의 Strict Mode가 활성화되어있음
+  // Strict Mode는 useEffect를 두 번 실행해 부작용이 있나 확인함...
+  // 따라서 useEffect가 두 번 실행되는걸 막기 위해 useRef를 사용
+  useEffect(() => {
+    if (!isAuthenticated && !redirected.current) {
+      redirected.current = true;
+      alert("로그인을 먼저 해주세요!");
+      router.push("/");
+    }
+  }, [isAuthenticated, router]);
+
+  // 이게 없으면 일단 writeLetter 페이지로 이동하고 alert이 뜬다...
   if (!isAuthenticated) {
-    return <Navigate to="/" replace />;
+    return null;
   }
 
   return children;
