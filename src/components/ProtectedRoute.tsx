18f9deecdceeec21812d96d490971880727156a0
yongaricode <kang061313@gmail.com>
2024-12-14 20:04:04 +0900

    fix: alert이 두 번 뜨는 오류 해결

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -6,10 +6,16 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   // hydration error가 떠서 초기 렌더링 시 아무것도 렌더링하지 않도록 수정
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
   const router = useRouter();
+  const redirected = useRef(false);
 
+  // useRef 처음에 안썼더니, useEffect가 두 번 실행돼 alert이 두 번 뜨는 상황이 발생
+  // 검색해보니, next는 개발 모드에서 React의 Strict Mode가 활성화되어있음
+  // Strict Mode는 useEffect를 두 번 실행해 부작용이 있나 확인함...
+  // 따라서 useEffect가 두 번 실행되는걸 막기 위해 useRef를 사용
   useEffect(() => {
     const token = Cookies.get("accessToken");
-    if (!token) {
+    if (!token && !redirected.current) {
+      redirected.current = true;
       alert("로그인을 먼저 해주세요!");
       router.push("/");
       setIsAuthenticated(false);
